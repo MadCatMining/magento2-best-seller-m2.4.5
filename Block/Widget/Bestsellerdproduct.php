1eda2042dcaa37d66adab2d12a6353a001babe65
MadCatMining <zuklys@mail.ru>
2025-10-26 01:45:19 +0300

    Updated composer.json

--- Block/Widget/Bestsellerdproduct.php
+++ Block/Widget/Bestsellerdproduct.php
@@ -1,149 +1,189 @@
 <?php
 namespace Emizentech\Bestsellerwidget\Block\Widget;
 
-class Bestsellerdproduct extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+use Magento\Catalog\Block\Product\Context;
+use Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory as BestsellersCollectionFactory;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\Product\Visibility;
+
+class Bestsellerdproduct extends Template implements BlockInterface
 {
-	protected $_template = 'widget/bestsellerdproduct.phtml';
-	 
-	    /**
-     * Default value for products count that will be shown
-     */
+    protected $_template = 'Emizentech_Bestsellerwidget::widget/bestsellerdproduct.phtml';
+
     const DEFAULT_PRODUCTS_COUNT = 10;
-     const DEFAULT_IMAGE_WIDTH = 150;
-     const DEFAULT_IMAGE_HEIGHT = 150;
-    /**
-     * Products count
-     *
-     * @var int
-     */
-    protected $_productsCount;
-    protected $_productloader;
-    /**
-     * @var \Magento\Framework\App\Http\Context
-     */
-    protected $httpContext;
-    protected $_resourceFactory;
-    /**
-     * Catalog product visibility
-     *
-     * @var \Magento\Catalog\Model\Product\Visibility
-     */
-    protected $_catalogProductVisibility;
-    
-	/**
-     * Product collection factory
-     *
-     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
-     */
-    protected $_productCollectionFactory;
-    
-    /**
-     * Image helper
-     *
-     * @var Magento\Catalog\Helper\Image
-     */
-    protected $_imageHelper;
-     /**
-     * @var \Magento\Checkout\Helper\Cart
-     */
-    protected $_cartHelper;
-    /**
-     * @param Context $context
-     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
-     * @param \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility
-     * @param \Magento\Framework\App\Http\Context $httpContext
-     * @param array $data
-     */
-   public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Reports\Model\ResourceModel\Report\Collection\Factory $resourceFactory,
-        \Magento\Reports\Model\Grouped\CollectionFactory $collectionFactory,
-        \Magento\Reports\Helper\Data $reportsData,
-        \Magento\Catalog\Model\ProductFactory $_productloader,
+    const DEFAULT_IMAGE_WIDTH = 150;
+    const DEFAULT_IMAGE_HEIGHT = 150;
+    const DEFAULT_PRODUCTS_PER_SLIDE = 4;
+    const DEFAULT_AUTOPLAY_SPEED = 3000;
+    const DEFAULT_PERIOD = 'monthly';
+
+    protected $imageHelper;
+    protected $cartHelper;
+    protected $bestsellersCollectionFactory;
+    protected $productRepository;
+    protected $productVisibility;
+    protected $storeManager;
+
+    public function __construct(
+        Context $context,
+        BestsellersCollectionFactory $bestsellersCollectionFactory,
+        ProductRepository $productRepository,
+        Visibility $productVisibility,
         array $data = []
     ) {
-        $this->_resourceFactory = $resourceFactory;
-        $this->_collectionFactory = $collectionFactory;
-        $this->_reportsData = $reportsData;
-        $this->_imageHelper = $context->getImageHelper();
-        $this->_cartHelper = $context->getCartHelper();
-        $this->_productloader = $_productloader;
+        $this->imageHelper = $context->getImageHelper();
+        $this->cartHelper = $context->getCartHelper();
+        $this->bestsellersCollectionFactory = $bestsellersCollectionFactory;
+        $this->productRepository = $productRepository;
+        $this->productVisibility = $productVisibility;
+        $this->storeManager = $context->getStoreManager();
         parent::__construct($context, $data);
     }
- 	/**
-     * Image helper Object
-     */
- 	public function imageHelperObj(){
-        return $this->_imageHelper;
+
+    public function imageHelperObj()
+    {
+        return $this->imageHelper;
     }
-    /**
-     * get featured product collection
-     */
-    public function getBestsellerProduct(){
+
+    public function getBestsellerProduct()
+    {
+        $limit = $this->getProductLimit();
+        $period = $this->getPeriod();
+
+        $collection = $this->bestsellersCollectionFactory->create();
+
+        $dateRange = $this->getDateRangeByPeriod($period);
+        if ($dateRange['from'] && $dateRange['to']) {
+            $collection->addFieldToFilter('period', ['from' => $dateRange['from'], 'to' => $dateRange['to']]);
+        }
+
+        $collection->setPageSize($limit * 2);
+
+        return $collection;
+    }
+
+    protected function getDateRangeByPeriod($period)
+    {
+        $timezone = new \DateTimeZone($this->_localeDate->getConfigTimezone());
+        $to = new \DateTime('now', $timezone);
+        $from = clone $to;
+
+        switch ($period) {
+            case 'daily':
+                $from->modify('-1 day');
+                break;
+            case 'yearly':
+                $from->modify('-1 year');
+                break;
+            case 'monthly':
+            default:
+                $from->modify('-1 month');
+                break;
+        }
+
+        return [
+            'from' => $from->format('Y-m-d'),
+            'to' => $to->format('Y-m-d')
+        ];
+    }
+
+    public function getValidBestsellerProducts()
+    {
+        $collection = $this->getBestsellerProduct();
+        $validProducts = [];
         $limit = $this->getProductLimit();
 
-		$resourceCollection = $this->_resourceFactory->create('Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection');
-		$resourceCollection->setPageSize($limit);
-		return $resourceCollection;
+        foreach ($collection as $item) {
+            if (count($validProducts) >= $limit) {
+                break;
+            }
+
+            try {
+                $product = $this->productRepository->getById(
+                    $item->getProductId(),
+                    false,
+                    $this->storeManager->getStore()->getId()
+                );
+
+                if ($product->getId() &&
+                    $product->getStatus() == \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED &&
+                    in_array($product->getVisibility(), [
+                        Visibility::VISIBILITY_BOTH,
+                        Visibility::VISIBILITY_IN_CATALOG,
+                        Visibility::VISIBILITY_IN_SEARCH
+                    ])
+                ) {
+                    $validProducts[] = $product;
+                }
+            } catch (NoSuchEntityException $e) {
+                continue;
+            }
+        }
+
+        return $validProducts;
     }
 
-    /**
-        *Get Prduct by id
-    */
-    public function getLoadProduct($id){
-        return $this->_productloader->create()->load($id);
-    }
-   
-    /**
-     * Get the configured limit of products
-     * @return int
-     */
-    public function getProductLimit() {
-		if($this->getData('productcount')==''){
-			return self::DEFAULT_PRODUCTS_COUNT;
-		}
-        return $this->getData('productcount');
-    }
-     /**
-     * Get the widht of product image
-     * @return int
-     */
-    public function getProductimagewidth() {
-		if($this->getData('imagewidth')==''){
-			return self::DEFAULT_IMAGE_WIDTH;
-		}
-        return $this->getData('imagewidth');
-    }
-     /**
-     * Get the height of product image
-     * @return int
-     */
-    public function getProductimageheight() {
-		if($this->getData('imageheight')==''){
-			return self::DEFAULT_IMAGE_HEIGHT;
-		}
-        return $this->getData('imageheight');
-    }
-    /**
-     * Get the add to cart url
-     * @return string
-     */
-     public function getAddToCartUrl($product, $additional = [])
+    public function getLoadProduct($id)
     {
-			return $this->_cartHelper->getAddUrl($product, $additional);
+        try {
+            return $this->productRepository->getById($id, false, $this->storeManager->getStore()->getId());
+        } catch (NoSuchEntityException $e) {
+            return null;
+        }
     }
-     /**
-     * Return HTML block with price
-     *
-     * @param \Magento\Catalog\Model\Product $product
-     * @param string $priceType
-     * @param string $renderZone
-     * @param array $arguments
-     * @return string
-     * @SuppressWarnings(PHPMD.NPathComplexity)
-     */
+
+    public function getProductLimit()
+    {
+        $count = $this->getData('productcount');
+        return $count ? (int)$count : self::DEFAULT_PRODUCTS_COUNT;
+    }
+
+    public function getProductimagewidth()
+    {
+        $width = $this->getData('imagewidth');
+        return $width ? (int)$width : self::DEFAULT_IMAGE_WIDTH;
+    }
+
+    public function getProductimageheight()
+    {
+        $height = $this->getData('imageheight');
+        return $height ? (int)$height : self::DEFAULT_IMAGE_HEIGHT;
+    }
+
+    public function isCarouselEnabled()
+    {
+        return (bool)$this->getData('enable_carousel');
+    }
+
+    public function getProductsPerSlide()
+    {
+        $perSlide = $this->getData('products_per_slide');
+        return $perSlide ? (int)$perSlide : self::DEFAULT_PRODUCTS_PER_SLIDE;
+    }
+
+    public function getAutoplaySpeed()
+    {
+        $speed = $this->getData('autoplay_speed');
+        return $speed ? (int)$speed : self::DEFAULT_AUTOPLAY_SPEED;
+    }
+
+    public function getPeriod()
+    {
+        $period = $this->getData('period');
+        return $period ?: self::DEFAULT_PERIOD;
+    }
+
+    public function getAddToCartUrl($product, $additional = [])
+    {
+        return $this->cartHelper->getAddUrl($product, $additional);
+    }
+
     public function getProductPriceHtml(
-        \Magento\Catalog\Model\Product $product,
+        ProductInterface $product,
         $priceType = null,
         $renderZone = \Magento\Framework\Pricing\Render::ZONE_ITEM_LIST,
         array $arguments = []
@@ -151,20 +191,11 @@ class Bestsellerdproduct extends \Magento\Framework\View\Element\Template implem
         if (!isset($arguments['zone'])) {
             $arguments['zone'] = $renderZone;
         }
-        $arguments['zone'] = isset($arguments['zone'])
-            ? $arguments['zone']
-            : $renderZone;
-        $arguments['price_id'] = isset($arguments['price_id'])
-            ? $arguments['price_id']
-            : 'old-price-' . $product->getId() . '-' . $priceType;
-        $arguments['include_container'] = isset($arguments['include_container'])
-            ? $arguments['include_container']
-            : true;
-        $arguments['display_minimal_price'] = isset($arguments['display_minimal_price'])
-            ? $arguments['display_minimal_price']
-            : true;
 
-            /** @var \Magento\Framework\Pricing\Render $priceRender */
+        $arguments['price_id'] = $arguments['price_id'] ?? 'old-price-' . $product->getId() . '-' . $priceType;
+        $arguments['include_container'] = $arguments['include_container'] ?? true;
+        $arguments['display_minimal_price'] = $arguments['display_minimal_price'] ?? true;
+
         $priceRender = $this->getLayout()->getBlock('product.price.render.default');
 
         $price = '';
@@ -175,6 +206,16 @@ class Bestsellerdproduct extends \Magento\Framework\View\Element\Template implem
                 $arguments
             );
         }
+
         return $price;
+    }
+
+    public function getCarouselConfig()
+    {
+        return [
+            'enabled' => $this->isCarouselEnabled(),
+            'productsPerSlide' => $this->getProductsPerSlide(),
+            'autoplaySpeed' => $this->getAutoplaySpeed()
+        ];
     }
 }
